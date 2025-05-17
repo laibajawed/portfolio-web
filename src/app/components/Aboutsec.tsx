066d8255d531270c4a86644c003eb3bee6592e3f
laiba jawed <laib.a.jaweddd@gmail.com>
2025-05-17 09:04:10 +0500

    minor changes

--- src/app/components/Aboutsec.tsx
+++ src/app/components/Aboutsec.tsx
@@ -51,7 +51,7 @@ const AboutSection = () => {
           <p className="text-base lg:text-lg">
             I am a full stack web developer with a passion for creating
             interactive and responsive web applications. I have experience
-            working with TypeScript, Next.js, HTML, CSS, Tailwind CSS and Git. I am a quick learner and I am always
+            working with TypeScript, Python,Next.js, HTML, CSS, Tailwind CSS and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications.
           </p>
